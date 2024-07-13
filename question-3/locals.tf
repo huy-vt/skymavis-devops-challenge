947c77ac7ad82242435cb96cbd375f320c88d04a
huy-vo1-cake <huy.vo1@cake.vn>
2024-07-13 17:46:05 +0700

    tf fmt

--- question-3/locals.tf
+++ question-3/locals.tf
@@ -4,10 +4,7 @@ locals {
       for k, v in yamldecode(file(resource_file)) : v.name => v
     }
   ]...)
-  # resources_users = distinct(flatten([
-  #   for team in yamldecode(file("${path.cwd}/matrix.yaml")) :
-  #   team.member
-  # ]))
+
   resources_users = distinct(flatten([
     for team in yamldecode(file("${path.cwd}/matrix.yaml")) : concat(try(team.member, []), try(team.maintainer, []))
   ]))
