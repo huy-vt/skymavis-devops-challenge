24c3deae5f1b5d9b3f5be4d1bd4f4d85274c027f
huy-vo1-cake <huy.vo1@cake.vn>
2024-07-13 17:45:28 +0700

    Cover if create group not has member

--- question-3/locals.tf
+++ question-3/locals.tf
@@ -4,18 +4,22 @@ locals {
       for k, v in yamldecode(file(resource_file)) : v.name => v
     }
   ]...)
+  # resources_users = distinct(flatten([
+  #   for team in yamldecode(file("${path.cwd}/matrix.yaml")) :
+  #   team.member
+  # ]))
   resources_users = distinct(flatten([
-    for team in yamldecode(file("${path.cwd}/matrix.yaml")) :
-    team.member
+    for team in yamldecode(file("${path.cwd}/matrix.yaml")) : concat(try(team.member, []), try(team.maintainer, []))
   ]))
+
   team_members = flatten([
     for team in local.resources_github : [
-      for member in team.member : "${team.name}:${member}"
+      for member in try(team.member, []) : "${team.name}:${member}"
     ]
   ])
   team_maintainers = flatten([
     for team in local.resources_github : [
-      for member in team.maintainer : "${team.name}:${member}"
+      for member in try(team.maintainer, []) : "${team.name}:${member}"
     ]
   ])
 }
